581dac2ae566a4468b7ae50e1fa2eeec5b8799dc
guidonegri <bokita_93@hotmail.com>
2017-10-02 16:35:48 -0300

    Starting parseDB. Add Rule and Fact

--- src/interpreter.js
+++ src/interpreter.js
@@ -1,12 +1,50 @@
 var Interpreter = function () {
+  
+	var Fact = function (name, params) {
+	    this.name = name;
+	    this.params = params;
+	    this.getName = function () { return this.name; }
+	    this.getParams = function () { return this.params; }
+	}
 
-    this.parseDB = function (params, paramss, paramsss) {
+	var Rule = function (name, params, facts) {
+	    this.name = name;
+	    this.params = params;
+	    this.facts = facts;
+	    this.getName = function () { return this.name; }
+	    this.getParams = function () { return this.params; }
+	    this.getFacts = function () { return this.facts; }
+	}
+  
+	var db_facts = [];
+	var db_rules = [];
+  
+	var fact_pattern = /(.*)\((.*)\)\./;
+	var rule_pattern = /(.*)\((.*)\) :- (.*)\./;
 
-    }
+	this.parseDB = function (db) {
+		let count = 0;
+		for (i = 0; i < db.length; i++) { 
+			if (rule_pattern.test(db[i]) ) {
+				let rule = new Rule("pepe", ["x", "y"]);
+				db_rules.push(rule);
+				count++;
+			} else if (fact_pattern.test(db[i]) ) {
+				let fact = new Fact("pepe", ["x", "y"]);
+					db_facts.push(fact);
+					count++;			
+				} else {
+					alert(db[i]);
+				}
+			}
+		if (count != db.length) {console.log("Error in the DB."); return null;}
+		else database = db;
+	}
 
-    this.checkQuery = function (params) {
-        return true;
-    }
+	this.checkQuery = function (query) {
+		//return database[0];
+		return true;
+	}
 
 }
 
