03be119edbc832608f2dd02015f33c329bae85fe
guidonegri <bokita_93@hotmail.com>
2017-10-07 23:32:24 -0300

    Add more acceptance test

---
+++ test/interpreterAcceptanceTests2.js
@@ -0,0 +1,78 @@
+var expect = require("chai").expect;
+var should = require('should');
+var assert = require('assert');
+
+var Interpreter = require('../src/interpreter');
+
+
+describe("Interpreter", function () {
+
+    var db = [
+		"add(zero, zero, zero).",
+		"add(zero, one, one).",
+		"add(zero, two, two).",
+		"add(one, zero, one).",
+		"add(one, one, two).",
+		"add(one, two, three).",
+		"add(two, zero, two).",
+		"add(two, one, three).",
+		"add(two, two, four).",
+		"subtract(X, Y, Z) :- add(Y, Z, X)."
+    ];
+
+    var interpreter = null;
+
+    before(function () {
+        // runs before all tests in this block
+    });
+
+    after(function () {
+        // runs after all tests in this block
+    });
+
+    beforeEach(function () {
+        // runs before each test in this block
+        interpreter = new Interpreter();
+        interpreter.parseDB(db);
+    });
+
+    afterEach(function () {
+        // runs after each test in this block
+    });
+
+    describe('Interpreter Facts', function () {
+
+        it("add(one, one, two) should be true", function () {
+            assert(interpreter.checkQuery("add(one, one, two)"));
+        });
+
+        it("add(two, one, one) should be false", function () {
+            assert(interpreter.checkQuery("add(two, one, one)") === false);
+        });
+        
+        it("add(two, two, four) should be true", function () {
+            assert(interpreter.checkQuery("add(two, two, four)"));
+        }); 
+
+    });
+    
+    describe('Interpreter Rules', function () {
+
+        it("subtract(one, one, two) should be false", function () {
+            assert(interpreter.checkQuery("subtract(one, one, two)") === false);
+        });
+        
+        it("subtract(two, one, one) should be true", function () {
+            assert(interpreter.checkQuery("subtract(two, one, one)"));
+        });
+        
+        it("subtract(one, zero, one) should be true", function () {
+            assert(interpreter.checkQuery("subtract(one, zero, one)"));
+        });
+
+    });
+
+
+});
+
+
